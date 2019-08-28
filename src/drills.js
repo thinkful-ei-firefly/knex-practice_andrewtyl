9c4126c1d6c1622658df2b7afe5a47f10af4a050
andrewtyl <andrewtyl@gmail.com>
2019-08-28 13:27:01 -0500

    finished drills

--- src/drills.js
+++ src/drills.js
@@ -11,3 +11,52 @@ const knexInstance = knex({
 });
 
 console.log('connection successful');
+
+
+function searchName(searchTerm) {
+  knexInstance.from('shopping_list')
+    .select('name', 'price', 'category', 'checked', 'date_added')
+    .from('shopping-list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+function paginateAll(pageNum) {
+  const itemsPerPage = 6;
+  const offset = itemsPerPage * (pageNum - 1);
+  knexInstance
+    .select('name', 'price', 'category', 'checked', 'date_added')
+    .from('shopping_list')
+    .limit(itemsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+function itemsAddedAfter(daysAgo) {
+  let cutoff = `now() - '${daysAgo} days'::INTERVAL`;
+  knexInstance
+    .select('name', 'price', 'category', 'checked', 'date_added')
+    .from('shopping_list')
+    .where('date_added', '>=', knexInstance.raw(cutoff))
+    .then(result => {
+      console.log(result);
+    });
+}
+
+function totalCost() {
+  knexInstance
+    .select('category')
+    .sum('price as total')
+    .from('shopping_list')
+    .groupBy('category')
+    .then(result => {
+      console.log('COST PER CATEGORY');
+      console.log(result);
+    });
+}
+
+totalCost();
